b118a95991897c48f5c63b251c2060349353ed17
David Bahtiar <52740811+davidbahtiar55@users.noreply.github.com>
2023-10-14 18:48:13 +0700

    Push code golang

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("David")
+	fmt.Println("Bahtiar")
+	fmt.Println("David Bahtiar")
+
+	fmt.Println(len("David"))
+	fmt.Println("Bahtiar"[0])
+	fmt.Println("David Bahtiar"[1])
+}
---
+++ vairable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "David Bahtiar"
+	fmt.Println(name)
+
+	name = "Ayana Bahtiar"
+	fmt.Println(name)
+
+	var friendName = "Budi"
+	fmt.Println(friendName)
+
+	var age = 26
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "David Bahtiar"
+		lastName  = "Dewi"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
